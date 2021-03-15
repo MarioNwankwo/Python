492f58cc0d40a4d14767130fe88774328723a631
Mario <marionwankwo2005@icloud.com>
2021-03-15 19:08:43 +0800

    second commit

--- week.py
+++ week.py
@@ -1,5 +1,5 @@
-first_name = 'James'
-last_name = 'Bond'
+first_name = 'John'
+last_name = 'Cena'
 age = 55
 
 sentence = 'The name is '+last_name + '. '+first_name + ' ' + last_name + '. And I am ' + str(age)
